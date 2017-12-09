6f592ab19596e82b1c87b04e75bc6b4f3e59cd20
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-09 17:56:10 -0600

    Fix repeatString file

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-var repeatString = function() {
-
+var repeatString = function(string, repeatTimes) {
+  if (repeatTimes < 0) return "ERROR";
+  let repeatString = "";
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatString += string;
+  }
+  return repeatString;
 }
 
 module.exports = repeatString
