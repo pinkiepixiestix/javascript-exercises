36f60c193e2d404aa5a45cb23bfd3bd6df45c199
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-09 19:24:24 -0600

    Fix sumall file

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-var sumAll = function() {
+var sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') return ("ERROR");
+  let sum = 0;
+  let largerNumber = 0;
+  let smallerNumber = 0;
 
+  if (num1 > num2) { largerNumber = num1; smallerNumber = num2; }
+  else { largerNumber = num2; smallerNumber = num1; }
+
+  for (let i = smallerNumber; i <= largerNumber; i++) sum += i;
+
+  return sum;
 }
 
 module.exports = sumAll
