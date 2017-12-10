31a6ba146141205d1bbefe632119e961cd742c7b
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-09 18:38:24 -0600

    Fix removeFromArray file

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-var removeFromArray = function() {
+var removeFromArray = function(arr) {
+  var args = Array.prototype.slice.call(arguments);
 
+  for (let i = 1; i < args.length; i++) {
+    let removal = args[i];
+    let j = 0;
+    while(arr.length > 0 && j < arr.length) {
+      if (arr[j] == removal) {
+        arr.splice(j, 1);
+      }
+      j++;
+    }
+  } // end for args
+  return arr;
 }
 
 module.exports = removeFromArray
