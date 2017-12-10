02c2118f366b64e3db1e3ff7b1e9d949f8a840aa
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-09 19:52:00 -0600

    Fix tempConversion file

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-var ftoc = function() {
-  
+var ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  if (celsius - parseInt(celsius) !== 0) celsius = parseFloat(celsius.toFixed(1));
+
+  return celsius;
 }
 
-var ctof = function() {
-  
+var ctof = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  if (fahrenheit - parseInt(fahrenheit) !== 0) fahrenheit = parseFloat(fahrenheit.toFixed(1));
+
+  return fahrenheit;
 }
 
 module.exports = {
