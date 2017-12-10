def55f5a984294606f1131c4917105a615687dc8
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-09 19:29:56 -0600

    Fix leapYears file

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-var leapYears = function() {
-
+/* There is a leap year every year whose number is perfectly divisible by
+four - except for years which are both divisible by 100 and not divisible by 400.
+*/
+var leapYears = function(year) {
+  return (year % 4 === 0 && !(year % 100 === 0 && year % 400 !== 0));
 }
 
 module.exports = leapYears
